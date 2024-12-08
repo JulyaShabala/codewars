e27d05f12dd0f119f48c189722ce03f60afdeb10
Julya Shabala <shabala.julya@gmail.com>
2024-12-08 16:37:36 +0300

    complete sum-mixed-array

---
+++ sum-mixed-array.js
@@ -0,0 +1,13 @@
+//DESCRIPTION
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+
+//TESTS
+// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+//     assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+//     assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+//SOLUTION
+sumMix = x => x.reduce((acc, val) => + val + acc, 0) 
+
--- terminal-game-move-function.js
+++ terminal-game-move-function.js
@@ -17,3 +17,4 @@ function move (position, roll) {
 }
 
 
+
